cea09fbf2d69d7202d02a9ad30d72b04bee88afd
JJMayorgaB <125193201+JJMayorgaB@users.noreply.github.com>
2023-02-28 09:07:40 -0500

    Update funcionpade.cpp

--- funcionpade.cpp
+++ funcionpade.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
 
+double sin_pade(double x){
+
+    double a = ((12671/4363920)*(std::pow(x, 5)) - ((2363/18183)*(std::pow(x, 3))) + x);
+    double b = (1 + ((445/12122)*std::pow(x, 2))  + ((601/872784)*std::pow(x, 4)) + ((121/16662240)*std::pow(x, 6)));
+
+    return a/b;
+}
+
+int main(void){
+
+    double x = 0.1;
+    std::cout << x <<"\t"<< sin_pade(x) << std::sin(x) << std::endl;
+    
+    return 0;
+}
